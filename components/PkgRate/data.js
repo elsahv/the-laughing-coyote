a0dd49daa421fa2016e831595d3ee37a4d0c3b85
Elsa <elsahvy@gmail.com>
2023-03-14 14:11:27 -0700

    rm blog service option

--- components/PkgRate/data.js
+++ components/PkgRate/data.js
@@ -14,19 +14,19 @@ const content = [
     listItem4: "Contact Component",
     listItem5: "Design and Hosting Assistance/ Education",
   },
-  {
-    id: 2,
-    image: ImageTwo,
-    number: 2,
-    title: "Blog",
-    price: "from $299",
-    listItem1: "Home pg- Single pg with all info",
-    listItem2: "About Component",
-    listItem3: "Contact Component",
-    listItem4: "Single Post Slug",
-    listItem5: "Design and Hosting Assistance/ Education",
-    //use gomez
-  },
+  // {
+  //   id: 2,
+  //   image: ImageTwo,
+  //   number: 2,
+  //   title: "Blog",
+  //   price: "from $299",
+  //   listItem1: "Home pg- Single pg with all info",
+  //   listItem2: "About Component",
+  //   listItem3: "Contact Component",
+  //   listItem4: "Single Post Slug",
+  //   listItem5: "Design and Hosting Assistance/ Education",
+  //   //use gomez
+  // },
 ];
 
 export default content;
